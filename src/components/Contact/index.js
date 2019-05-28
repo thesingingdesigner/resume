53cb74cdb937236c9ca444a1fb1be4330f6099ab
thesingingdesigner <thesingingdesigner@gmail.com>
2019-05-28 21:06:11 +0800

    testing of contact form

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -6,32 +6,17 @@ const Contact = () => {
     <Container>
       <Row>
         <Col sm="12" md={{ size: 6, offset: 3 }}>
-          <Form
-            style={{ marginBottom: '40px' }}
-            name="contact"
-            method="post"
-            action="/success"
-            data-netlify="true"
-            data-netlify-honeypot="bot-field"
-          >
-            <input type="hidden" name="bot-field" />
-            <FormGroup>
-              <Label for="email">Email</Label>
-              <Input type="email" name="email" id="email" placeholder="Email" />
-            </FormGroup>
-            <FormGroup>
-              <Label for="subject">Subject</Label>
-              <Input type="text" name="subject" id="subject" placeholder="Subject" />
-            </FormGroup>
-            <FormGroup>
-              <div data-netlify-recaptcha="true"></div>
-            </FormGroup>
-            <FormGroup>
-              <Label for="message">Text Area</Label>
-              <Input type="textarea" name="message" id="message" placeholder="Message" />
-            </FormGroup>
-            <Button>Submit</Button>
-          </Form>
+        <form name="contact" netlify>
+  <p>
+    <label>Name <input type="text" name="name" /></label>
+  </p>
+  <p>
+    <label>Email <input type="email" name="email" /></label>
+  </p>
+  <p>
+    <button type="submit">Send</button>
+  </p>
+</form>
         </Col>
       </Row>
     </Container>
